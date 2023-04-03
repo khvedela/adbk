fb9e1e856c0c37f5c6d2e7b0afb1463ee0153eb8
david khvedelidze <d.khv08@gmail.com>
2023-04-03 19:14:30 +0400

    'init'

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { Alert } from 'flowbite-svelte';
+    import { Progressbar } from 'flowbite-svelte'
+</script>
+
+<div class="p-8 main">
+	<Alert>
+		<span class="font-medium">Hello 👋 !</span> This is an official archive website for <b><a href="https://www.instagram.com/ananobandzeladze/">Ano</a></b> and <b><a href="https://www.instagram.com/khveedelidze/">Data</a></b>
+	</Alert>
+    <Progressbar progress="2" labelOutside="Website done" />
+</div>
+
+
+<style>
+    a {
+        text-decoration: underline;
+    }
+</style>
